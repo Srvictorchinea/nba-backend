3e8d47bdfc5dae548717cb6d035c49479c2a826f
Srvictorchinea <sr.victorchinea@gmail.com>
2024-04-14 23:16:14 +0100

    #7 crear rutas para team

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/teams', function () {
+Route::get('teams', function () {
     return Team::all(); 
 });
 
@@ -12,17 +12,17 @@ Route::get('/teams/{id}', function ($id) {
     return Team::find($id);
 });
 
-Route::post('/teams', function (Request $request) {
-    return Team::create($request->all);
+Route::post('teams', function (Request $request) {
+    return Team::create($request->all());
 });
 
-Route::put('/teams/{id}', function (Request $request, $id) {
+Route::put('teams/{id}', function (Request $request, $id) {
     $team = Team::findOrFail($id);
     $team->update($request->all());
     return $team;
 });
 
-Route::delete('team/{id}', function ($id) {
+Route::delete('teams/{id}', function ($id) {
     Team::find($id)->delete();
     return 204;
 });
