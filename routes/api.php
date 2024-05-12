48fb471ce852108bb5fab994204fd0462e6d9e5f
Srvictorchinea <sr.victorchinea@gmail.com>
2024-05-12 21:51:31 +0100

    feature/auth-jwt finished

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeamController;
+use App\Http\Controllers\UserController;
 
 Route::get('teams',[TeamController::class, 'index']);
 Route::get('/teams/{team}',[TeamController::class, 'show']);
 Route::post('/teams',[TeamController::class, 'store']);
 Route::put('/teams/{team}',[TeamController::class, 'update']);
 Route::delete('/teams/{team}',[TeamController::class, 'remove']);
+
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($routes) {
+    Route::post('signup', [UserController::class, 'userRegister']);
+    Route::post('signin', [UserController::class, 'userLogin']);
+});
